77a001af414a22a7b46cfcefbdfde7a2b120e851
Nathan Oehlman <nathan.oehlman@sidelab.com>
2014-02-07 12:43:10 +1000

    Update argument handling

--- index.js
+++ index.js
@@ -6,14 +6,12 @@ function buildArgs(opts) {
         options = _.omit(opts, 'destination');
     for (name in options) {
       var value = options.hasOwnProperty(name) ? options[name] : defaults.hasOwnProperty(name) ? defaults[name] : null;
-      if (value === true) {
         args.push('--' + name)
-      } else if (value !== false) {
-        args.push('--' + name + '=' + value);
+      if (value) {
+        args.push(value);
       }
     }
 
-    args.push((opts.destination ? opts.destination : '-'));
     return args;
 }
 
@@ -39,8 +37,11 @@ var defaults = module.exports.defaults = function(defaults) {
         throw "Unsupported format. Use 1 of pdf or png"
       }
 
-      var args = buildArgs(_.extend(defaults, opts || {}));
-      return spawn(executable, [input || '-'].concat(args), env || defaultEnv);
+      var args = buildArgs(_.extend(defaults, opts || {})),
+          destination = (opts.destination ? opts.destination : '-'),
+          launchArgs = args.concat([input || '-', destination]);
+
+      return spawn(executable, launchArgs, env || defaultEnv);
     }
   }
 }
